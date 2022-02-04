ae779a817f12195f2cf5d30ee0cc2891e6624ba9
silversurfer98 <sraghul360@gmail.com>
2022-02-04 17:35:11 +0530

    finds fractional power

---
+++ power_func_driver.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include"power.h"
+
+// refer exeptions, this is very primitive
+// does not handle NaN and inf ot 0's
+
+// this can calculate ln(x) in float precision
+// this can calculate exp(x) in float precision
+// call appropriate function in order to get values
+
+// AGAIN THIS IS VERY PRIMITIVE OMALE BE CAREFUL
+
+
+int main()
+{
+	float x, y;
+	std::cout << "\n\n***** to find power *******\n\n";
+	std::cout << " Enter a : ";
+	std::cin >> x;
+	std::cout << " Enter b : ";
+	std::cin >> y;
+	std::cout << "\n\n The ans to " << x << " raised to power of " << y << " is : " << mpower(x, y) << "\n\n";
+	std::cout << "\n\n The ans to " << x << " raised to power of " << y << " is : " << mexp(x) << "\n\n";
+	return 0;
+}
